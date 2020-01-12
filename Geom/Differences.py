3e24c886b7ab59f175cc5b3906a5ed4435c79565
Nikita Shubin <50959311+YourMonika@users.noreply.github.com>
2020-01-13 02:39:47 +0300

    add 3d part. VarMeansDispersions

---
+++ Geom/Differences.py
@@ -0,0 +1,11 @@
+num = [5, 10, 100, 1000, 10000]
+for i in range(5):
+    disp = []
+    data = []
+    for j in range(len(num)):
+        with open("Geom{}_{}.txt".format(num[i], j+1)) as f:
+            for line in f:
+                data.append(list([float(x) for x in line.split()]))
+    for k in range(len(data)):
+        mean = sum(data[k]) / len(data[k])
+        print("Разница при n = {}:".format(len(data[k])), abs(0.2 - 1/(1+mean)))
